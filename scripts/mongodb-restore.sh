713a8be0c431e426ce7ebb2c9fabd60e9fc5d5f7
lpm0073 <lpm0073@gmail.com>
2022-12-14 15:10:44 -0600

    refactor

--- scripts/mongodb-restore.sh
+++ scripts/mongodb-restore.sh
@@ -1,27 +1,43 @@
-# openedx-mongo-20220912T080001.tgz
+#!/bin/bash
+#------------------------------------------------------------------------------
+# written by:   Lawrence McDaniel
+#               https://lawrencemcdaniel.com
+#
+# date:         dec-2022
+#
+# usage:        download a MongoDB tarball backup from AWS S3
+#               decompress the contents
+#------------------------------------------------------------------------------
 
-MONGODB_HOST="mongodb.master.app.turnthebus.org:27017"
+MONGODB_HOST="mongo"
 S3_BUCKET="ttb-india-prod-backup"
-BACKUP_KEY="20221213T171420"
+BACKUP_KEY="20221214T161333"
 
 BACKUP_TARBALL="openedx-mongo-$BACKUP_KEY.tgz"
 BACKUP_FILE="mongo-dump-$BACKUP_KEY"
+BACKUPS_DIRECTORY="/home/ubuntu/backups/"
 
-if [ ! -f "~/backups/mongodb/$BACKUP_TARBALL" ]; then
-    #aws s3 cp s3://$S3_BUCKET/backups/$BACKUP_TARBALL ~/backups/mongodb/ --recursive
-    aws s3 cp s3://$S3_BUCKET/backups/$BACKUP_TARBALL ~/backups/mongodb/
+#Check to see if a backups/ folder exists. if not, create it.
+if [ ! -d ${BACKUPS_DIRECTORY} ]; then
+    mkdir ${BACKUPS_DIRECTORY}
+    echo "created backups folder ${BACKUPS_DIRECTORY}"
 fi
 
-if [ ! -f "~/backups/mongodb/$BACKUP_FILE" ]; then
+if [ ! -f "${BACKUPS_DIRECTORY}${BACKUP_TARBALL}" ]; then
+    #aws s3 cp s3://$S3_BUCKET/backups/$BACKUP_TARBALL ${BACKUPS_DIRECTORY} --recursive
+    aws s3 cp s3://$S3_BUCKET/backups/$BACKUP_TARBALL ${BACKUPS_DIRECTORY}
+fi
+
+if [ ! -f "${BACKUPS_DIRECTORY}${BACKUP_FILE}" ]; then
     echo "decompressing..."
-    cd ~/backups/mongodb
+    cd ${BACKUPS_DIRECTORY}
     tar xvzf $BACKUP_TARBALL
     cd ~
 fi
 
 mongo $MONGODB_HOST
-use ttb_prod_edx;
-db.dropDatabase();
+`use ttb_prod_edx`;
+`db.dropDatabase()`;
 exit
 
-mongorestore -d ttb_prod_edx ~/backups/mongodb/mongo-dump-${BACKUP_KEY}/edxapp --host $MONGODB_HOST
+mongorestore -d ttb_prod_edx ${BACKUPS_DIRECTORY}mongo-dump-${BACKUP_KEY}/edxapp 

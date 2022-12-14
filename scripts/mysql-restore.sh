975c06ec8cae91c9587cf3300bfd6d8628c96c78
lpm0073 <lpm0073@gmail.com>
2022-12-14 13:05:40 -0600

    CREATE DATABASE ttb_prod_edx CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci

--- scripts/mysql-restore.sh
+++ scripts/mysql-restore.sh
@@ -42,5 +42,5 @@ echo "importing to $MYSQL_HOST"
 mysql -h $MYSQL_HOST  -u $MYSQL_ROOT_USERNAME -p$MYSQL_ROOT_PASSWORD < ~/backups/$BACKUP_FILE
 
 echo "migrating openedx database"
-mysql -h $MYSQL_HOST -u $MYSQL_ROOT_USERNAME -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS ttb_prod_edx; CREATE DATABASE ttb_prod_edx CHARACTER SET utf8 COLLATE utf8_general_ci;"
+mysql -h $MYSQL_HOST -u $MYSQL_ROOT_USERNAME -p$MYSQL_ROOT_PASSWORD -e "DROP DATABASE IF EXISTS ttb_prod_edx; CREATE DATABASE ttb_prod_edx CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;"
 mysqldump --set-gtid-purged=OFF --column-statistics=0 -h $MYSQL_HOST -u $MYSQL_ROOT_USERNAME -p$MYSQL_ROOT_PASSWORD openedx | mysql -h $MYSQL_HOST  -u $MYSQL_ROOT_USERNAME -p$MYSQL_ROOT_PASSWORD -D ttb_prod_edx
